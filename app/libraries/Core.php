d415d06723ecba5bd35981305c6e0f1d591f7a28
edwardcolley <edward.colley@outlook.com>
2019-12-05 14:08:08 +0000

    dev

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -6,7 +6,8 @@
     * URL format - /controller/method/params
 */
 
-class Core {
+class Core
+{
 
     protected $currentController = 'Pages';
     protected $currentMethod = 'index';
@@ -20,7 +21,7 @@ class Core {
 
         // Look in controllers for first value/index
 
-        if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
+        if (file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
 
             //If exists, then set as the $currentController
             $this->currentController = ucwords($url[0]);
@@ -34,10 +35,31 @@ class Core {
 
         //Instantiate controller class
         $this->currentController = new $this->currentController;
+
+        //Check for second part of URL (method)
+        if (isset($url[1])) {
+            //Check to see if method exists in controller
+            if (method_exists($this->currentController, $url[1])) {
+                $this->currentMethod = $url[1];
+                //unset $url[1] index
+                unset($url[1]);
+
+            }
+        }
+
+        //Get Params
+        $this->params = $url ? array_values($url) : [];
+
+        //Call a callback with an array of Params
+        call_user_func_array([
+            $this->currentController,
+            $this->currentMethod
+        ], $this->params);
     }
 
-    public function getUrl() {
-        if(isset($_GET['url'])) {
+    public function getUrl()
+    {
+        if (isset($_GET['url'])) {
             $url = rtrim($_GET['url'], '/');
             $url = filter_var($url, FILTER_SANITIZE_URL);
             $url = explode('/', $url);
