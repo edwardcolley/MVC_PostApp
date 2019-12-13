649e3a8195b5f03e263d913467b5be2e676b523f
edwardcolley <edward.colley@outlook.com>
2019-12-13 13:45:26 +0000

    setup database communication with PDO

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -4,13 +4,16 @@ class Pages extends Controller {
     
     public function __construct()
     {
-        // echo 'Pages loaded...';
+        $this->postModel = $this->model('Post');
     }
-
+    
     public function index() {
+        $posts = $this->postModel->getPosts();
         $data = [
-            'title' => 'Welcome'
+            'title' => 'Welcome',
+            'posts' => $posts
         ];
+
         $this->view('pages/index', $data);
     }
 
